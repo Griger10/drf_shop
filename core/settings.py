2212a0b7cce4f42dce0f78c64cfaebc9c22a9e3f
Grigorii <griger1011@gmail.com>
2025-01-30 22:35:48 +0300

    Added skeleton of the project

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.accounts',
+    'apps.shop',
+    'apps.profiles',
+    'apps.sellers'
 ]
 
 MIDDLEWARE = [
