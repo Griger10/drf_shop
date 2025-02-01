d199f2dce9fd256fd3558871d145b158ecb3beeb
Grigorii <griger1011@gmail.com>
2025-02-01 19:25:24 +0300

    Added models for accounts and one for seller

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+from apps.accounts.managers import CustomUserManager
+from apps.common.models import IsDeletedModel, BaseModel
+
+ACCOUNT_TYPE_CHOICES = (
+    ("SELLER", "SELLER"),
+    ("BUYER", "BUYER"),
+)
+
+
+class User(AbstractBaseUser, IsDeletedModel):
+    first_name = models.CharField(verbose_name="First name", max_length=25, null=True)
+    last_name = models.CharField(verbose_name="Last name", max_length=25, null=True)
+    email = models.EmailField(verbose_name="Email address", unique=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, default='avatars/default.jpg')
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    account_type = models.CharField(max_length=6, choices=ACCOUNT_TYPE_CHOICES, default="BUYER")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_superuser(self):
+        return self.is_staff
+
+
+class ShippingAddress(BaseModel):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="shipping_addresses"
+    )
+    full_name = models.CharField(max_length=1000)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=1000, null=True)
+    city = models.CharField(max_length=200, null=True)
+    country = models.CharField(max_length=200, null=True)
+    zipcode = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.full_name}'s shipping details"
