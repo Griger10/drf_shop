0f19ba00ca8bc8e6129257067a1a756f9a1e4865
Grigorii <griger1011@gmail.com>
2025-02-10 10:35:10 +0300

    updated docs schema

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,18 +1,35 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.common.utils import set_dict_attr
 from apps.profiles.serializers import ProfileSerializer
 
+tags = ['Profiles']
+
 
 class ProfileView(APIView):
     serializer_class = ProfileSerializer
 
+    @extend_schema(
+        summary="Retrieve profile",
+        description="""
+                This endpoint allows a user to retrieve his/her profile.
+            """,
+        tags=tags,
+    )
     def get(self, request):
         user = request.user
         serializer = self.serializer_class(user)
         return Response(data=serializer.data, status=200)
 
+    @extend_schema(
+        summary="Update profile",
+        description="""
+            This endpoint allows to update changeable profile info.
+        """,
+        tags=tags
+    )
     def put(self, request):
         user = request.user
         serializer = self.serializer_class(data=request.data)
@@ -22,6 +39,13 @@ class ProfileView(APIView):
         serializer = self.serializer_class(user)
         return Response(data=serializer.data)
 
+    @extend_schema(
+        summary="Delete profile",
+        description="""
+            This endpoint allows to do "safe" deletion of profile
+        """,
+        tags=tags
+    )
     def delete(self, request):
         user = request.user
         user.is_active = False
